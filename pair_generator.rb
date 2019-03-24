b011271991ab4126201a05c0a30e039155e1770d
sharolyn evans <sharolynevans@hotmail.com>
2019-03-24 17:35:27 +0000

    Automatically backed up by Learn

---
+++ pair_generator.rb
@@ -0,0 +1,6 @@
+def random_pairs2(names)
+  until randomized_students.empty?
+  student1
+tudents = ["sharolyn", "jalen"," "mary","leland","deka", "jacob", "ronnie", "alex', "jacoby", "aeisha","reina"]
+
+
